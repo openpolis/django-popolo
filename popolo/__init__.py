a463bceb79275c2344ce050b38f318c0eeb55dd9
guglielmo <guglielmo.celata@gmail.com>
2017-09-16 01:14:02 +0200

    package description changed

--- popolo/__init__.py
+++ popolo/__init__.py
@@ -1,3 +1,3 @@
-"A Django-based implementation of the Popolo data specifications."
+"A Django-based implementation of the Popolo data specifications, usinc numerical PKs for major entities."
 
 __version__ = '1.1.0'
