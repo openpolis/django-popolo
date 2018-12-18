d917cda162e7c08bcecf040feda29b0d858f2d47
guglielmo <guglielmo.celata@gmail.com>
2018-12-18 19:43:04 +0100

    HistoricAreaManager created for extraction of historical data about areas

--- popolo/models.py
+++ popolo/models.py
@@ -137,7 +137,7 @@ class OtherNamesShortcutsMixin(object):
             i, created = self.other_names.get_or_create(
                 othername_type=othername_type,
                 name=name,
-                **kwargs
+                defaults=kwargs
             )
             return i
         else:
@@ -2517,6 +2517,108 @@ class ContactDetail(
         return u"{0} - {1}".format(self.value, self.contact_type)
 
 
+class HistoricAreaManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def comuni_with_prov_and_istat_identifiers(self, d):
+        """Return the list of comuni active at a given date,
+        with the province id, name and identifier, plus the ISTAT_CODE_COM identifier valid at a given time
+
+        :param d: the date
+        :return: a list of Area objects, annotated with these fields:
+         - istat_identifier
+         - prov_id
+         - prov_name
+         - prov_identifier
+        """
+        ar_qs = AreaRelationship.objects.filter(classification='FIP')
+        ar_qs = \
+            ar_qs.filter(start_date__lte=d, end_date__gte=d) | \
+            ar_qs.filter(start_date__lte=d, end_date__isnull=True) | \
+            ar_qs.filter(start_date__isnull=True, end_date__gte=d) | \
+            ar_qs.filter(start_date__isnull=True, end_date__isnull=True)
+
+        prev_provs = {
+            a['source_area_id']: {
+                'prov_id': a['dest_area_id'],
+                'prov': a['dest_area__identifier'],
+                'prov_name': a['dest_area__name']
+            } for a in ar_qs.values(
+                'source_area_id', 'source_area__name',
+                'dest_area_id', 'dest_area__name', 'dest_area__identifier'
+            )
+        }
+
+        current_provs = {
+            a['id']: {
+                'prov_id': a['parent_id'],
+                'prov': a['parent__identifier'],
+                'prov_name': a['parent__name']
+            } for a in Area.objects.comuni().current(d).values(
+                'id', 'parent_id', 'parent__name', 'parent__identifier'
+            )
+        }
+
+        provs = {}
+        for a_id, prov_info in current_provs.items():
+            if a_id in prev_provs:
+                prov_id = prev_provs[a_id]['prov_id']
+                prov = prev_provs[a_id]['prov']
+                prov_name = prev_provs[a_id]['prov_name']
+            else:
+                prov_id = current_provs[a_id]['prov_id']
+                prov = current_provs[a_id]['prov']
+                prov_name = current_provs[a_id]['prov_name']
+
+            provs[a_id] = {
+                'prov_id': prov_id,
+                'prov': prov,
+                'prov_name': prov_name
+
+            }
+
+        current_comuni_qs = Area.objects.comuni().current(moment=d)
+        current_comuni_qs = current_comuni_qs.\
+            filter(
+                identifiers__scheme='ISTAT_CODE_COM',
+                identifiers__start_date__lte=d, identifiers__end_date__gte=d
+            ) | \
+        current_comuni_qs.\
+            filter(
+                identifiers__scheme='ISTAT_CODE_COM',
+                identifiers__start_date__lte=d, identifiers__end_date__isnull=True
+            ) | \
+        current_comuni_qs.\
+            filter(
+                identifiers__scheme='ISTAT_CODE_COM',
+                identifiers__start_date__isnull=True, identifiers__end_date__isnull=True
+            ) | \
+        current_comuni_qs.\
+            filter(
+                identifiers__scheme='ISTAT_CODE_COM',
+                identifiers__start_date__isnull=True, identifiers__end_date__gte=d
+            )
+
+        current_comuni = current_comuni_qs.values(
+            'id', 'name', 'identifiers__identifier',
+        )
+
+        results_list = []
+        for com in current_comuni:
+            c = self.model(
+                id=com['id'], name=com['name']
+            )
+            c.istat_identifier = com['identifiers__identifier']
+            c.prov_name = provs[com['id']]['prov_name']
+            c.prov_id = provs[com['id']]['prov_id']
+            c.prov_identifier = provs[com['id']]['prov']
+
+            results_list.append(c)
+
+        return results_list
+
+
 @python_2_unicode_compatible
 class Area(
     SourceShortcutsMixin, LinkShortcutsMixin,
@@ -2700,6 +2802,8 @@ class Area(
         objects = PassThroughManager.for_queryset_class(AreaQuerySet)()
     except:
         objects = AreaQuerySet.as_manager()
+
+    historic_objects = HistoricAreaManager()
 
     def add_i18n_name(self, name, language):
         """add an i18 name to the area
