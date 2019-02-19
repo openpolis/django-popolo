ceff451c30d2b0b2c5cd58f377577e326c73aafd
Gabriele Lucci <gabriele@lucci.xyz>
2019-02-19 13:39:43 +0100

    Update popolo module

--- popolo/__init__.py
+++ popolo/__init__.py
@@ -1,3 +1,3 @@
-"A Django-based model of the OpenGovernment context, compliant with the Popolo data specifications."
+"""A Django-based implementation of the OpenGovernment context, compliant with the Popolo data specifications."""
 
 __version__ = '2.4.0'
