e6fdd5161394b18fed0acd53261667fd998ab168
Gabriele Lucci <gabriele@lucci.xyz>
2019-02-19 13:40:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Framework :: Django",
         "Framework :: Django :: 1.10",
         "Framework :: Django :: 1.11",
