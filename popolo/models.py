4783743af70a728adab9a49e6cfe0a38ff9f1dee
guglielmo <guglielmo.celata@gmail.com>
2019-09-04 18:12:13 +0200

    HistoricAreaManager.comuni_with_prov_and_istat_identifiers method improved

--- popolo/models.py
+++ popolo/models.py
@@ -2406,16 +2406,25 @@ class HistoricAreaManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset()
 
-    def comuni_with_prov_and_istat_identifiers(self, d):
+    def comuni_with_prov_and_istat_identifiers(self, d, filters=None, excludes=None):
         """Return the list of comuni active at a given date,
-        with the province id, name and identifier, plus the ISTAT_CODE_COM identifier valid at a given time
+        with the name and identifier, plus the ISTAT_CODE_COM identifier valid at a given time
+        and province and region information
 
         :param d: the date
+        :param filters: a list of filters, in the form of dict
+        :param excludes: a list of exclude in the form of dict
         :return: a list of Area objects, annotated with these fields:
+         - id
+         - identifier
+         - name
          - istat_identifier
          - prov_id
          - prov_name
          - prov_identifier
+         - reg_id
+         - reg_name
+         - reg_identifier
         """
         ar_qs = AreaRelationship.objects.filter(classification="FIP")
         ar_qs = (
@@ -2433,6 +2442,17 @@ class HistoricAreaManager(models.Manager):
             }
             for a in ar_qs.values(
                 "source_area_id", "source_area__name", "dest_area_id", "dest_area__name", "dest_area__identifier"
+            )
+        }
+
+        current_regs = {
+            a["id"]: {
+                "reg_id": a["parent__parent_id"],
+                "reg_identifier": a["parent__parent__identifier"],
+                "reg_name": a["parent__parent__name"]
+            }
+            for a in Area.objects.comuni().current(d).values(
+                "id", "parent__parent_id", "parent__parent__name", "parent__parent__identifier"
             )
         }
 
@@ -2472,15 +2492,27 @@ class HistoricAreaManager(models.Manager):
             )
         )
 
+        if filters:
+            for f in filters:
+                current_comuni_qs = current_comuni_qs.filter(**f)
+        if excludes:
+            for e in excludes:
+                current_comuni_qs = current_comuni_qs.exclude(**e)
+
         current_comuni = current_comuni_qs.values("id", "name", "identifier", "identifiers__identifier")
 
         results_list = []
         for com in current_comuni:
-            c = self.model(id=com["id"], name=com["name"])
+            c = self.model(
+                id=com["id"], name=com["name"], identifier=com["identifier"]
+            )
             c.istat_identifier = com["identifiers__identifier"]
             c.prov_name = provs[com["id"]]["prov_name"]
             c.prov_id = provs[com["id"]]["prov_id"]
             c.prov_identifier = provs[com["id"]]["prov"]
+            c.reg_name = current_regs[com["id"]]["reg_name"]
+            c.reg_id = current_regs[com["id"]]["reg_id"]
+            c.reg_identifier = current_regs[com["id"]]["reg_identifier"]
 
             results_list.append(c)
 
