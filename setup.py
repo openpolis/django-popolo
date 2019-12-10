f60fc12daa0fe6e1c361a614d9247c4d4661c607
Gabriele Lucci <gabriele@lucci.xyz>
2019-12-10 16:12:02 +0100

    Refresh setup script

--- setup.py
+++ setup.py
@@ -1,52 +1,52 @@
-import os
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+from setuptools import setup
 
-
-def read_file(filename):
-    """Read a file into a string"""
-    path = os.path.abspath(os.path.dirname(__file__))
-    filepath = os.path.join(path, filename)
-    try:
-        return open(filepath).read()
-    except IOError:
-        return ""
-
-
-setup_requirements = []
-install_requirements = ["django-autoslug==1.9.4", "django-model-utils"]
-test_requirements = ["Django==1.11", "Faker==0.8.13", "factory_boy==2.10.0", "mock==2.0.0"]
+import popolo
 
 setup(
     name="django-popolo",
-    version=__import__("popolo").__version__,
+    version=popolo.__version__,
     author="Guglielmo Celata",
     author_email="guglielmo@openpolis.it",
-    packages=find_packages(),
+    packages=["popolo"],
     include_package_data=True,
     url="http://github.com/openpolis/django-popolo",
-    license="Affero",
-    description=u" ".join(__import__("popolo").__doc__.splitlines()).strip(),
+    license="AGPL-3.0",
+    description=popolo.__doc__,
     classifiers=[
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Framework :: Django",
-        "Framework :: Django :: 1.10",
         "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.2",
         "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
     ],
-    long_description=read_file("README.md"),
-    test_suite="runtests.runtests",
+    long_description=open("./README.md").read(),
+    long_description_content_type="text/markdown",
     zip_safe=False,
-    tests_require=test_requirements,
-    install_requires=install_requirements,
-    setup_requires=setup_requirements,
+    install_requires=(
+        # Project requirements (semi-pinned)
+        "django<2.3",  # Django 2.2 LTS - https://github.com/django/django
+        "django-autoslug<2",  # https://github.com/justinmayer/django-autoslug
+        "django-model-utils<4",  # https://github.com/jazzband/django-model-utils
+    ),
+    extras_require={
+        "test": (
+            # Test requirements
+            "faker",  # https://github.com/joke2k/faker
+            "factory_boy",  # https://github.com/FactoryBoy/factory_boy
+        ),
+        "dev": (
+            # Development requirements
+            "black",  # https://github.com/psf/black
+            "coverage",  # https://github.com/nedbat/coveragepy
+        ),
+    },
 )
