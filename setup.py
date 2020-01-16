9fc25fe9b91d33b138e183517b6322385af92398
Gabriele Lucci <gabriele@lucci.xyz>
2020-01-16 19:09:32 +0100

    setup: Update classifiers

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Framework :: Django",
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.2",
@@ -32,7 +33,7 @@ setup(
     long_description_content_type="text/markdown",
     zip_safe=False,
     install_requires=(
-        # Project requirements (semi-pinned)
+        # Package requirements (pin major versions)
         "django<2.3",  # Django 2.2 LTS - https://github.com/django/django
         "django-autoslug<2",  # https://github.com/justinmayer/django-autoslug
         "django-model-utils<4",  # https://github.com/jazzband/django-model-utils
