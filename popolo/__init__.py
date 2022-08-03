ca6c9958da5df72b9197bc0944969fb354aa0d3a
guglielmo <guglielmo@openpolis.it>
2022-08-03 11:58:18 +0200

    Bump version: 3.0.4 → 3.0.5

--- popolo/__init__.py
+++ popolo/__init__.py
@@ -1,5 +1,5 @@
 """A Django-based implementation of the OpenGovernment context, compliant with the Popolo data specifications."""
 
-__version__ = "3.0.4"
+__version__ = "3.0.5"
 
 default_app_config = "popolo.apps.PopoloConfig"
